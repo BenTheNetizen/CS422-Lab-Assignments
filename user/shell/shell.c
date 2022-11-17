fc8063b690c1917a4041a936621bea31dd7e5b33
Chen Benjamin <blc38@zoo.cs.yale.edu>
2022-11-17 16:04:31 -0500

    finished shell parsing

--- user/shell/shell.c
+++ user/shell/shell.c
@@ -3,32 +3,140 @@
 #include <syscall.h>
 #include <x86.h>
 
-const int STATE_LS = 0;
-const int STATE_PWD = 1;
-const int STATE_CD = 2;
-const int STATE_CP = 3;
-const int STATE_MV = 4;
-const int STATE_RM = 5;
-const int STATE_MKDIR = 6;
-const int STATE_CAT = 7;
-const int STATE_TOUCH = 8;
-const int STATE_PARSE = 9;
+// const int STATE_LS = 0;
+// const int STATE_PWD = 1;
+// const int STATE_CD = 2;
+// const int STATE_CP = 3;
+// const int STATE_MV = 4;
+// const int STATE_RM = 5;
+// const int STATE_MKDIR = 6;
+// const int STATE_CAT = 7;
+// const int STATE_TOUCH = 8;
+// const int STATE_PARSE = 9;
 
+const int STATE_COMMAND = 0;
+const int STATE_FLAG = 1;
+const int STATE_PARAM = 2;
+const int STATE_PARSE = 4;
+const int STATE_INIT = 5;
+
+void parse(char* buff, char* command, char* flag, char* param1, char* param2) {
+  int STATE = STATE_INIT;
+  char *init_command = command;
+  char *init_flag = flag;
+  char *init_param1 = param1;
+  char *init_param2 = param2;
+  char *curr_command = command;
+  char *curr_flag = flag;
+  char *curr_param1 = param1;
+  char *curr_param2 = param2;
+
+  int has_param1 = 0;
+  int has_param2 = 0;
+  int has_flag = 0;
+
+  while (*buff != '\0') {
+    switch (STATE) {
+      case STATE_INIT:
+        if (*buff != ' ') {
+          *curr_command = *buff;
+          curr_command++;
+          STATE = STATE_COMMAND;
+        }
+        break;
+      case STATE_PARSE:
+        if (*buff == '-' && !has_flag) {
+          *curr_flag= *buff;
+          curr_flag++;
+          STATE = STATE_FLAG;
+        } else if (*buff != ' ') {
+          if (!has_param1) {
+            *curr_param1= *buff;
+            curr_param1++;
+          } else {
+            *curr_param2 = *buff;
+            curr_param2++;
+          }
+          STATE = STATE_PARAM;
+        }
+        break;
+      case STATE_COMMAND:
+        if (*buff == ' ') {
+          // *command = '\0';
+          STATE = STATE_PARSE;
+        } else {
+          *curr_command= *buff;
+          curr_command++;
+        }
+        break;
+      case STATE_FLAG:
+        if (*buff == ' ') {
+          // *flag = '\0';
+          has_flag = 1;
+          STATE = STATE_PARSE;
+        } else {
+          *curr_flag = *buff;
+          curr_flag++;
+        }
+        break;
+      case STATE_PARAM:
+        if (*buff == ' ') {
+          if (!has_param1) {
+            // *param1 = '\0';
+            has_param1 = 1;
+          } else if (!has_param2) {
+            // *param2 = '\0';
+            has_param2 = 1;
+          }
+          STATE = STATE_PARSE;
+        } else {
+          if (!has_param1) {
+            *curr_param1 = *buff;
+            curr_param1++;
+          } else if (!has_param2) {
+            *curr_param2 = *buff;
+            curr_param2++;
+          }
+        }
+        break;
+    }
+    buff++;
+  }
+
+  // reset pointers
+  strncpy(command, init_command, strlen(init_command));
+  strncpy(flag, init_flag, strlen(init_flag));
+  strncpy(param1, init_param1, strlen(init_param1));
+  strncpy(param2, init_param2, strlen(init_param2));
+
+  command[strlen(init_command)] = '\0';
+  flag[strlen(init_flag)] = '\0';
+  param1[strlen(init_param1)] = '\0';
+  param2[strlen(init_param2)] = '\0';
+}
 
 int main(int argc, char **argv)
 {
-  int STATE = STATE_PARSE;
   printf("shell\n");
-  char *s = "prompt>";
+  char s[10] = "prompt>";
   char buff[128];
-  readline(buff, s);
+  char command[128];
+  char flag[128];
+  char param1[128];
+  char param2[128];
 
-  printf("readline: %s\n", buff);
+  while(1) {
+    memset(buff, 0, 128);
+    memset(command, 0, 128);
+    memset(flag, 0, 128);
+    memset(param1, 0, 128);
+    memset(param2, 0, 128);
+    readline(buff, s);
 
-  unsigned int index = 0;
-  char curr[128];
-  switch(STATE) {
-    case STATE_PARSE:
-      
+    printf("readline: %s\n", (char*)buff);
+
+    parse(buff, command, flag, param1, param2);
+
+    printf("parsed: command: %s, flag: %s, param1: %s, param2: %s\n", command, flag, param1, param2);
   }
 }
